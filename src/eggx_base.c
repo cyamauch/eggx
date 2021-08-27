35031e59a4270464ee255fa173054e765e1b5be7
cyamauch <cyamauch@ir.isas.jaxa.jp>
2021-08-28 03:58:47 +0900

    Version 0.95.

--- src/eggx_base.c
+++ src/eggx_base.c
@@ -1,8 +1,8 @@
 /* -*- Mode: C ; Coding: euc-japan -*- */
-/* Time-stamp: <2020-09-19 12:00:00 cyamauch> */
+/* Time-stamp: <2021-08-28 03:57:41 cyamauch> */
 
 /*
-  EGGX / ProCALL  version 0.94
+  EGGX / ProCALL  version 0.95
                    eggx_base.c
   究極の簡単さを目指して作成した，C ，FORTRAN 両用の
   X11グラフィックスライブラリ．
